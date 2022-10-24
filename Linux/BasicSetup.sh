c0320db1aced877cf4b38f715f3c4e8f863d984b
Selkeph <selkeph@pop-os.localdomain>
2022-10-25 00:24:50 +0100

    Linux added.

--- Linux/BasicSetup.sh
+++ Linux/BasicSetup.sh
@@ -12,4 +12,17 @@ sudo timeshift --create --comments "First Backup"
 
 # Now upgrade and add applications
 sudo apt upgrade
-sudo apt install git vim firefox discord papirus-icon-theme gnome-tweaks gnome-shell-extension-manager htop
+sudo apt install git vim firefox discord papirus-icon-theme gnome-tweaks gnome-shell-extension-manager htop -y
+
+# Visual Studio Code
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+# Add pop-shell and disable window titles
+
