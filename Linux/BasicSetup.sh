f870cf1158fc8ab47aa6c67e0207d7f6837d144a
Selkeph <selkeph@pop-os.localdomain>
2023-01-04 15:01:20 +0000

    OnlyOffice

--- Linux/BasicSetup.sh
+++ Linux/BasicSetup.sh
@@ -92,3 +92,21 @@ echo ----------------------------------------------
 flatpak install flathub md.obsidian.Obsidian
 
 # Only-Office
+echo ----------------------------------------------
+echo BasicSetup.sh \\ Installing Only-Office...
+echo ----------------------------------------------
+mkdir -p ~/.gnupg
+chmod 700 ~/.gnupg
+gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+chmod 644 /tmp/onlyoffice.gpg
+sudo chown root:root /tmp/onlyoffice.gpg
+sudo mv /tmp/onlyoffice.gpg /etc/apt/trusted.gpg.d/
+
+echo "deb https://download.onlyoffice.com/repo/debian squeeze main" | sudo tee /etc/apt/sources.list.d/onlyoffice.list
+
+sudo apt-get update
+
+sudo apt-get install ttf-mscorefonts-installer
+
+sudo apt-get install onlyoffice-documentserver
+
